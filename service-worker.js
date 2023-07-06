591bad5539b29416a78c4f62507844148dbab6c0
supermommy <78208406+huanghongbook@users.noreply.github.com>
2023-07-06 09:17:27 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  'styles.css'
+];
+
+self.addEventListener('install', function(event) {
+  // 执行安装步骤
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // 缓存中有匹配的则返回缓存，否则去请求网络
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
