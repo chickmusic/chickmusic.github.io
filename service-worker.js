acb0f9784e4e017f40bdea31be3de9f40f4e9a98
supermommy <78208406+huanghongbook@users.noreply.github.com>
2023-08-15 12:04:34 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'my-site-cache-v1';
+var CACHE_NAME = 'my-site-cache-v2';
 var urlsToCache = [
   '/',
   'styles.css'
@@ -16,36 +16,14 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  const url = new URL(event.request.url);
-
-  // 如果请求的是一个视频文件，就将其缓存起来
-  if (url.pathname.endsWith('.mp4')) {
-    event.respondWith(
-      caches.open('video-cache').then(cache => {
-        return cache.match(event.request).then(response => {
-          // 如果缓存中已经有这个视频，就直接返回
-          if (response) return response;
-
-          // 否则，就从网络获取并添加到缓存中
-          return fetch(event.request.clone()).then(networkResponse => {
-            if (networkResponse.ok) {
-              cache.put(event.request, networkResponse.clone());
-            }
-            return networkResponse;
-          });
-        });
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // 缓存中有匹配的则返回缓存，否则去请求网络
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
       })
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          // 缓存中有匹配的则返回缓存，否则去请求网络
-          if (response) {
-            return response;
-          }
-          return fetch(event.request);
-        })
-    );
-  }
+  );
 });
